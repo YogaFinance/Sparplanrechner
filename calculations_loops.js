80912d8b14fdd7b5f3c4643528c01fcd6c5f7cbd
YogaFinance <80750378+YogaFinance@users.noreply.github.com>
2021-03-16 10:18:30 +0100

    First upload of files
    Erste rudimentäre Version

---
+++ calculations_loops.js
@@ -0,0 +1,74 @@
+class Savings_Plan {
+    initialInvestment = 0;
+    dynamization = 0;
+
+    constructor(savingsRate, interestRate, timeHorizon) {
+        this.savingsRate = parseFloat(savingsRate);
+        this.interestRate = parseFloat(interestRate);
+        this.timeHorizon = parseFloat(timeHorizon);
+    }
+
+    createReturnVector() {
+        var returnVector = new Array(parseInt(this.timeHorizon * 12 + 1));
+        for (const i of returnVector.keys()) {
+            returnVector[i] = (1 + this.interestRate) ** (i / 12);
+        }
+        this.returnVector = returnVector;
+    }
+
+    createInvestmentsVector() {
+        var investments = new Array(parseInt(this.timeHorizon * 12 + 1));
+        if (this.dynamization == 0) {
+            investments.fill(this.savingsRate);
+        } else {
+            var dynsamicSavingsRate = this.savingsRate;
+            for (var i = 1; i < investments.length; i++) {
+                investments[i] = dynsamicSavingsRate;
+                if (i % 12 == 0) {
+                    dynsamicSavingsRate *= 1 + this.dynamization;
+                }
+            }
+        }
+        investments[0] = this.initialInvestment;
+        this.investments = investments;
+    }
+
+    createGrossNavHistory() {
+        this.createInvestmentsVector();
+        this.createReturnVector();
+        var grossNav = new Array(this.investments.length).fill(0);
+        for (const i of grossNav.keys()) {
+            for (var j = 0; j <= i; j++) {
+                grossNav[i] += this.investments[j] * this.returnVector[i - j];
+            }
+        }     
+        this.grossNav = grossNav;
+    }
+}
+
+function cumsum(list) {
+    let y = 0;
+    return list.map(d => y += d);
+}
+
+function execute() {
+    if (document.getElementById("Sparrate").value)
+    var Sparplan = new Savings_Plan(document.getElementById("Sparrate").value, 
+                                    document.getElementById("Zinsen").value / 100, 
+                                    document.getElementById("horizon").value);
+    Sparplan.initialInvestment = parseFloat(document.getElementById("Erstanlage").value);
+    Sparplan.dynamization = parseFloat(document.getElementById("Dynamisierung").value) / 100;
+    Sparplan.createGrossNavHistory();
+    var invCum = cumsum(Sparplan.investments);
+    // update chart
+    navChart.data.datasets[0].data = math.round(invCum,2);
+    navChart.data.datasets[1].data = math.round(math.subtract(Sparplan.grossNav, invCum),2);
+    navChart.data.labels = Array.from(Array(invCum.length).keys());
+    navChart.update();
+}
+
+document.addEventListener("keyup", function(evnt){
+    if ([9,13,48,49,50,51,52,53,54,55,56,57].includes(evnt.keyCode)) {
+        execute();
+    }
+});
---
+++ charts copy.js
@@ -0,0 +1,65 @@
+var myChart = document.getElementById('myChart').getContext('2d');
+
+// Global options
+Chart.defaults.global.defaultFontFamily = 'Helvetica';
+// Chart.defaults.global.defaultFontSize = 18;
+Chart.defaults.global.defaultFontColor = 'black';
+
+var navChart = new Chart(myChart, {
+    type:'bar',
+    data:{
+        labels: [0,1,2,3,4,5,6,7,8,9,10,11,12],
+        datasets: [{
+            label: "Investitionen",
+            data: [0,100,200,300,400,500,600,700,800,900,1000,1100,1200],
+            backgroundColor: 'green',
+        },  
+        {
+            label: "Zinsen",
+            data: [0,0,0.8,2.4,4.81,8.04,12.09,16.97,22.69,29.25,36.66,44.92,54.05],
+            backgroundColor: 'blue',
+        }]
+    },
+    options:{
+        title:{
+            display: false,
+            text: 'Sparplanrechner',
+            fontSize: 25
+        },
+        legend:{
+            position: 'right',
+            // labels: {
+            //     fontColor: 'red'
+            // }
+        },
+        scales: {
+            xAxes: [{
+                stacked: true,
+            }],
+            yAxes: [{
+                stacked: true,
+            }]
+        },
+        tooltips: {
+            mode: 'label',
+            callbacks: {
+                label: function(tooltipItem, data) {
+                    var corporation = data.datasets[tooltipItem.datasetIndex].label;
+                    var valor = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
+            
+                    // Loop through all datasets to get the actual total of the index
+                    var total = 0;
+                    for (var i = 0; i < data.datasets.length; i++)
+                        total += data.datasets[i].data[tooltipItem.index];
+            
+                    // If it is not the last dataset, you display it as you usually do
+                    if (tooltipItem.datasetIndex != data.datasets.length - 1) {
+                        return corporation + " : $" + valor.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+                    } else { // .. else, you display the dataset and the total, using an array
+                        return [corporation + " : $" + valor.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,'), "Gesamt : $" + total.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')];
+                    }
+               }
+            }
+        }
+    }
+});
